a36aa77a84f2f5694c3b29cd7956863e96894577
farhangoliloo <farhangoliloo@gmail.com>
2021-01-03 18:49:19 +0330

    change any file

--- Bitcoin_Price.py
+++ Bitcoin_Price.py
@@ -4,6 +4,7 @@ def Get_Price():
      
     res = requests.get("https://blockchain.info/ticker")
     response1 = res.json()['USD']['last']
-    response2 = res.json()['USD']["symbol"]
-    return response1,response2
+    #response2 = res.json()['USD']["symbol"]
+    return response1
+
         
